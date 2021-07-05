481a61a2ff65e45b00d0fbdb23d6a8f2993d7faa
RAza ALy <007razajutt@gmail.com>
2021-07-06 00:40:18 +0500

    finish slider btn effect

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,9 +2,21 @@ import React from "react";
 import "./Home.css";
 const Home = () => {
   return (
-      <div className="home">
-      <video src="./video/homepage.mp4" width="1024" height="543" loop autoPlay muted playsInline className="background_video"></video>
+    <div className="home">
+      <video
+        src="./video/homepage.mp4"
+        width="1024"
+        height="571"
+        loop
+        autoPlay
+        muted
+        playsInline
+        className="background_video"
+      ></video>
+      <div className="slider">
+        <div className="slider_btn"></div>
       </div>
+    </div>
   );
 };
 
