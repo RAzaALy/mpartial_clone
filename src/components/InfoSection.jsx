07620df4b89a2acb9680da57ff9cf0ffb458081a
RAza ALy <007razajutt@gmail.com>
2021-07-06 10:37:04 +0500

    compare panel layout 👍

--- src/components/InfoSection.jsx
+++ src/components/InfoSection.jsx
@@ -2,22 +2,48 @@ import React from "react";
 import "./InfoSection.css";
 const InfoSection = () => {
   return (
-    <div className="section">
-      <div className="left_title">
-        <h1 className="title">GROUND-TRUTH DATA EVERYONE TRUSTS</h1>
+    <>
+      <div className="section">
+        <div className="left_title">
+          <h1 className="title">GROUND-TRUTH DATA EVERYONE TRUSTS</h1>
+        </div>
+        <div className="right_content">
+          <p className="description">
+            We have combined the best-of-breed technology platforms with an eye
+            towards relieving you of administrative burden. Matterport 3D scans
+            augmented by TruePlan are used to generate consistent Xactimate
+            sheets that are delivered in accord with Actionable Insights
+            compliance rule sets. Each mpartial is produced with full
+            transparency, unprecedented forensic photography, and infallible
+            geospatial data that collectively result in rapid approvals.
+          </p>
+        </div>
       </div>
-      <div className="right_content">
-        <p className="description">
-          We have combined the best-of-breed technology platforms with an eye
-          towards relieving you of administrative burden. Matterport 3D scans
-          augmented by TruePlan are used to generate consistent Xactimate sheets
-          that are delivered in accord with Actionable Insights compliance rule
-          sets. Each mpartial is produced with full transparency, unprecedented
-          forensic photography, and infallible geospatial data that collectively
-          result in rapid approvals.
-        </p>
+      <div className="compare_panel">
+        <div className="container">
+          <img
+            alt="pre-Mitigation"
+            data-testid="right-image"
+            className="right_image"
+            src="./images/right-image.jpeg"
+          ></img>
+          <img
+            alt="post-Mitigation"
+            data-testid="left-image"
+            className="right_image"
+            src="./images/right-image.jpeg"
+          ></img>
+          <div className="line_container">
+            <div className="line-up">
+            </div>
+            <div className="arrow">
+              <img src="./images/arrow.png" alt="handle" loading="lazy" />
+            </div>
+            <div className="line-down"></div>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
